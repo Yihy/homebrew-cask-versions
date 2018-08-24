306ffdce8b9711d98e4a987bb23cf42d7a1f245d
赵建云 <jianyun.zhao@zhaopin.com.cn>
2018-08-25 01:33:22 +0800

    ADD:init

---
+++ Casks/termius.rb
@@ -0,0 +1,11 @@
+cask 'termius' do
+version :latest
+  sha256 :no_check
+
+  url "https://www.termius.com/download/mac/Termius.dmg"
+  name 'Termius'
+  homepage 'https://www.termius.com'
+
+  app 'Termius.app'
+
+end
