540fee38873e1495f44b913e92bb35fe94009af6
赵建云 <jianyun.zhao@zhaopin.com.cn>
2018-08-25 02:55:05 +0800

    FIX:修正校验码

--- Casks/shadowsocksx-ng-r8.rb
+++ Casks/shadowsocksx-ng-r8.rb
@@ -2,7 +2,7 @@ cask 'shadowsocksx-ng-r8' do
   version  '1.4.3-R8'
   sha256  '696f9e63370e6f100413c4d72fa807fa3ff80215d946a1fad15dfa4077b577ca'
 
-  url 'https://github.com/Yihy/Shell_Script/raw/master/apps/ShadowsocksX-NG-R8.dmg'
+  url 'https://github.com/Yihy/Shell_Script/raw/master/apps/ShadowsocksX-NG-R8.zip'
   name 'ShadowsocksX-NG-R8'
 
   app 'ShadowsocksX-NG-R8.app'
