accf1f8bb978c583c86ba87ecc99036fcc2dcaa1
赵建云 <jianyun.zhao@zhaopin.com.cn>
2018-08-25 02:45:36 +0800

    ADD:增ssr

---
+++ Casks/shadowsocksx-ng-r8.rb
@@ -0,0 +1,10 @@
+cask 'shadowsocksx-ng-r8' do
+  version  '1.4.3-R8'
+  sha256  'ad2d37f28fcb11f09ac26472ae00db38d0314b43a641600b08f2358f7420541b'
+
+  url 'https://sourcegraph.com/github.com/Yihy/Shell_Script@d513d4b1a34deacadd20ffdb3be8821fb09261fd/-/blob/apps/ShadowsocksX-NG-R8.dmg'
+  name 'ShadowsocksX-NG-R8'
+
+  app 'ShadowsocksX-NG-R8.app'
+
+end
